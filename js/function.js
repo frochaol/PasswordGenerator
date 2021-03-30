ce4738d29c606216cd5f6c9b75d3fbcf0a0449ee
Fernando Rocha Olivera <frochaolivera18@hotmail.com>
2021-03-29 21:09:47 -0500

    New functionality created for passwords rules

--- js/function.js
+++ js/function.js
@@ -1,3 +1,30 @@
+function convertPassword(x) {
+    // start at 22 because the password will have a lenght of 10 letters
+    var finalWord = '';
+    for (let step = 22; step < x.length; step++) {
+        const element = x.charAt(step);
+        var word = '';
+        // added key letters to complete passwords restrictions
+        switch (step) {
+            case 22:
+                word = '#';
+                break;
+            case 24:
+                word = 'F';
+                break;
+            case 31:
+                word = '%';
+                break;                
+            default:
+                word = x.charAt(step);
+                break;
+        }
+        finalWord = finalWord + word;
+        console.log(finalWord);
+    }
+    return finalWord;
+}
+
 function generateKey() 
 {
     var test = document.getElementById("keyword").value;
@@ -5,9 +32,10 @@ function generateKey()
     var conc = test+entity
     // -- Convert text to md5
     md5hash = md5(conc);
-    var result = md5hash;
-    document.getElementById("generated").value = result.substr(22, 10);    
-    document.getElementById("result").innerHTML = 'The key is generated' ;
+    console.log(md5hash);
+    var result = convertPassword(md5hash);
+    document.getElementById("generated").value = result;  
+    document.getElementById("result").innerHTML = 'The key was generated' ;
 }
 
 function clearText() {
@@ -16,3 +44,4 @@ function clearText() {
     document.getElementById("generated").value = '';
     document.getElementById("result").innerHTML = '' ;
 }
+
